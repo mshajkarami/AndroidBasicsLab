24d20e9c6d9df3bd6d4a3fea67ec56733c31c4ab
mshajkarami <mshajkarami.dev@gmail.com>
2025-02-18 08:29:21 +0330

    add intent and MainActivity2

---
+++ Generatingluckynumbers/app/src/main/java/ir/hajkarami/generatingluckynumbers/MainActivity2.kt
@@ -0,0 +1,36 @@
+package ir.hajkarami.generatingluckynumbers
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ir.hajkarami.generatingluckynumbers.databinding.ActivityMainBinding
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var userName = "saleh"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            if (! binding.editTextText.text.isEmpty())
+            userName = binding.editTextText.text.toString()
+            else{
+                userName = "salehHajkarami is Developer!"
+                Toast.makeText(this,"you do not enter your name !!",Toast.LENGTH_SHORT).show()
+            }
+            val intent : Intent = Intent(this,MainActivity2::class.java)
+            intent.putExtra("userName",userName)
+            startActivity(intent)
+        }
+    }
+
+}
