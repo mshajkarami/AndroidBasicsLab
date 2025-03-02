a5209a395d533b101743f514b6e65956a0e48b2d
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-02 12:23:02 +0330

    add to fragment_blank

--- FragmentsApp/app/src/main/java/ir/hajkarami/fragmentsapp/BlankFragment2.kt
+++ FragmentsApp/app/src/main/java/ir/hajkarami/fragmentsapp/BlankFragment2.kt
@@ -1,36 +1,24 @@
 package ir.hajkarami.fragmentsapp
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-class BlankFragment : Fragment() {
+
+class BlankFragment2 : Fragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        val view = inflater.inflate(R.layout.fragment_blank2, container, false)
+
+        return view
     }
-
-
 }
