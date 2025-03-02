7d2f2da2084f35e968a343ea0df3ec32a580d54c
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-02 08:22:00 +0330

    init Project

---
+++ Components/app/src/main/java/ir/hajkarami/components/MainActivity.kt
@@ -0,0 +1,21 @@
+package ir.hajkarami.components
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ir.hajkarami.components.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+    }
+}
