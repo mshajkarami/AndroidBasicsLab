bf3d7a07775b9b5099da81b351e9e86712712e18
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-02 11:48:11 +0330

    init Project

--- FragmentsApp/app/src/main/java/ir/hajkarami/fragmentsapp/MainActivity.kt
+++ FragmentsApp/app/src/main/java/ir/hajkarami/fragmentsapp/MainActivity.kt
@@ -1,12 +1,10 @@
-package ir.hajkarami.components_broadcast
+package ir.hajkarami.fragmentsapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import ir.hajkarami.components_broafcast.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +16,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val intent = Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-
-        val isAirplaneMode = intent.getBooleanExtra("state", false)
-        val msg = if (isAirplaneMode) "Airplane Mode Enabled" else "Airplane Mode Disabled"
-
-        println(msg)
     }
 }
