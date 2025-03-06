737e0d50be3db1efe14adc274eca960b4418acbb
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-06 09:40:56 +0330

    add TabLayout

--- ViewPager/app/src/main/java/ir/hajkarami/viewpager/MainActivity.kt
+++ ViewPager/app/src/main/java/ir/hajkarami/viewpager/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +23,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         viewPager = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
+
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
         val adapter = MyViewPagerAdapter(this)
         adapter.addFragment(Fragment1())
         adapter.addFragment(Fragment2())
         adapter.addFragment(Fragment3())
         viewPager.adapter = adapter
 
+        TabLayoutMediator(
+            tabLayout,
+            viewPager,
+            { tab, position ->
+                tab.setText("Fragment " + (position + 1))
+            }
+        ).attach()
     }
 }
