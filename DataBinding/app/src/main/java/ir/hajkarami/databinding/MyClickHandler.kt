ba0148f4e2240714298cb16c285baf9bd0eee7b4
mshajkarami <mshajkarami.dev@gmail.com>
2025-03-08 07:57:31 +0330

    add Data  Binding and Model class

---
+++ DataBinding/app/src/main/java/ir/hajkarami/databinding/MyClickHandler.kt
@@ -0,0 +1,12 @@
+package ir.hajkarami.databinding
+
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+
+class MyClickHandler(val context: Context) {
+
+    fun onButton1Clicked(view: View){
+        Toast.makeText(context,"First Button is Clicked",Toast.LENGTH_SHORT).show()
+    }
+}
