b2dd1491cb050d8d234c12740a7ca87788cd9752
mshajkarami <mshajkarami.dev@gmail.com>
2025-04-01 11:56:45 +0330

    Save first

--- FirebaseApp/app/src/main/java/ir/hajkarami/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/ir/hajkarami/firebaseapp/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // write a message to the database
+
+        // Initialize and Access the Firebase Database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        // Get a reference to a specific node in the database
+        DatabaseReference myRef = database.getReference("message");
+
+        // Write a value to the specified database location
+        myRef.setValue("Hello from our Course!");
+
     }
 }
