f6652360e5ed4df78a8bb7e42e3c44519f0a4ab7
mshajkarami <mshajkarami.dev@gmail.com>
2025-04-01 12:06:57 +0330

    setting Realtime Database

--- FirebaseApp/app/src/main/java/ir/hajkarami/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/ir/hajkarami/firebaseapp/MainActivity.java
@@ -1,15 +1,19 @@
 package ir.hajkarami.firebaseapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
 
         // Write a value to the specified database location
         myRef.setValue("Hello from our Course!");
+
+        TextView textView = findViewById(R.id.textView);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String newVale = snapshot.getValue(String.class);
+
+                textView.setText(newVale);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 }
